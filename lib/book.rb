956baf82130ad4e0ea24563f40cf8c4e05e8750b
Sean Oughton <seanoughton@gmail.com>
2018-02-03 00:42:11 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,6 +11,18 @@ class Book
     @author = author
   end
 
+  def author
+    @author
+  end
+
+  def page_count= (num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
 end
 
 Book.new("And Then There Were None")
